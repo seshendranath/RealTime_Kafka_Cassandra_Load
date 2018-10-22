7c30dd6ef4cc29f7d9433b13400bc0af495ba528
Ajay Guyyala <aguyyala@indeed.com>
2018-10-22 16:05:38 -0500

    edit check.sh

--- src/main/scripts/check.sh
+++ src/main/scripts/check.sh
@@ -5,7 +5,7 @@ job=$1
 echo "Started: "$job
 while [ True ]
 do
-    if [ `ps -ef | grep -v grep | grep "run.sh $job" | wc -l` -ne 1 ]
+    if [ `ps -ef | grep -v grep | grep "name $job" | wc -l` -ne 1 ]
     then
         echo "FAILED: "$job
         cat $job.log | mailx -a $job.log -s "Spark Streaming Job "$job" Failed" -r aguyyala@indeed.com aguyyala@indeed.com
